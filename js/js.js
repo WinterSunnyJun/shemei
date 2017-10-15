d05a15796af4ea2754e047a9ab6ad3e61afff41e
WinterSunnyJun <496900910@qq.com>
2017-10-15 14:25:12 +0800

    ajun

--- js/js.js
+++ js/js.js
@@ -1,11 +1,27 @@
-/**
- * Created by DELL on 2017/10/12.
- */
 $(function () {
-//    首页banner
+    //首页banner
     $(".banner-wrapper ol li").click(function () {
         var baNow = $(this).index();
         $(".banner-wrapper ol li").eq(baNow).addClass('active').siblings().removeClass('active');
         $(".banner-wrapper ul li").eq(baNow).show().siblings().hide();
     })
+//手机导航
+    $(".pho-nav-bar").click(function () {
+        if($(".pho-menu").css("display") == "block" ){
+            $(".pho-menu").slideUp();
+        }else {
+            $(".pho-menu").slideDown();
+        }
+    })
+//    联系我们标题
+    if ($(document.body).width() < 768 ){
+        $(".qywh-one-title").addClass('active');
+        $(".qywh-two-title").addClass('active');
+        $(".qywh-three-title").addClass('active');
+        $(".qywh-four-title").addClass('active');
+
+    }
+
+
+
 })
